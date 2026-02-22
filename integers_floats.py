086edfd4404715288afe01362b33df641a838811
Shawon <shawonh986@gmail.com>
2026-02-23 04:07:14 +0800

    all about integers and floats data type

---
+++ integers_floats.py
@@ -0,0 +1,34 @@
+num = 12
+
+print(type(num)) #this will print the type of the variable "num"
+
+# Arithmetic operators
+
+# Addition
+print(num + 5) #this will print the result of adding 5 to the variable "num"
+# Subtraction
+print(num - 5) #this will print the result of subtracting 5 from the variable "num"
+# Multiplication
+print(num * 5) #this will print the result of multiplying the variable "num" by 5
+# Division
+print(num / 5) #this will print the result of dividing the variable "num" by 5
+# Modulus
+print(num % 5) #this will print the result of the modulus operation, which gives the remainder of the division of the variable "num" by 5
+# Exponentiation
+print(num ** 2) #this will print the result of raising the variable "num" to the power of 2
+# Floor division
+print(num // 5) #this will print the result of floor division, which gives the quotient of the division of the variable "num" by 5, rounded down to the nearest whole number
+
+print(abs(-5)) #this will print the absolute value of -5, which is 5
+print(round(3.14159, 2)) #this will print the result of rounding the number 3.14159 to 2 decimal places, which is 3.14
+
+#Comparison operators
+print(num > 10) #this will print True if the variable "num" is greater than 10, otherwise it will print False
+print(num < 10) #this will print True if the variable "num" is less than 10, otherwise it will print False
+print(num == 12) #this will print True if the variable "num" is equal to 12, otherwise it will print False
+print(num != 12) #this will print True if the variable "num" is not equal to 12, otherwise it will print False
+
+num_1 = '100'
+num_2 = '200'
+print(num_1 + num_2) #this will print the result of concatenating the strings "100" and "200", which is "100200"
+print(int(num_1) + int(num_2)) #this will convert the strings "100" and "200" to integers and then print the result of adding them together, which is 300
